7d43a097857a7ab6ce5c786f7aafb9a2044ede6b
Saul <saul.perez-vallejo@marrquette.edu>
2024-04-23 15:24:33 -0500

    Create CustomerAppointmentClass

---
+++ CustomerAppointment.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassProject002;
+
+public class CustomerAppointment
+{
+    public Customer c {get; set;}
+    public Appointment a {get; set;}
+
+    public CustomerAppointment(Customer c, Appointment a)
+    {
+        this.c = c;
+        this.a = a;
+    }
+
+
+}
