2c594cd3f55c6c1b851d722b8e86806171bf1443
lade <omolade.akande@marquette.edu>
2024-04-23 14:55:54 -0500

    Create Customer class

---
+++ Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassProject002;
+
+public class Customer
+{
+
+    public int Id{get; set;}
+    public string FirstName{get; set;}
+    public string LastName{get; set;}
+    public string Username{get; set;}
+    public string Password{get; set;}
+}
