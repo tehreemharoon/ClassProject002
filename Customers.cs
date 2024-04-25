d75cc7717ec0ea3ffe110a77868c971f474a61df
Tehreem Haroon <tehreem.haroon@marquette.edu>
2024-04-25 15:08:02 -0500

    customers

---
+++ Customers.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassProject002;
+
+public class Customers
+{
+    public List<Customer> customerList { get; set; }
+    public Customers ()
+    {
+        customerList = new List<Customer>();
+    }
+    public Customer Authenticate(string username, string password)
+    {
+        var c = customerList.Where(o => (o.Username == username) && o.Password == password).ToList();
+        if (c.Count() > 0)
+        {
+            return c.First();
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+}
